0f6523db955d84a7d4322b0f9afbc9e04506b304
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-07-21 19:01:44 +0900

    Update ProgE1.java

--- src/main/java/ProgE1.java
+++ src/main/java/ProgE1.java
@@ -1,7 +1,12 @@
 public class ProgE1 {
 
     public static void main(String[] args) {
-
+        String str = null; // String型変数strの宣言と初期化
+        System.out.print("キーボードの確認：　1を押してエンターキーを押してください。:");
+        int choice = new java.util.Scanner(System.in).nextInt();
+        if(choice == 1) {
+            str = "生命太郎";
+        }
+        System.out.println("文字列は「生命太郎」。文字数は"+str.length()+"です。");
     }
 }
-
